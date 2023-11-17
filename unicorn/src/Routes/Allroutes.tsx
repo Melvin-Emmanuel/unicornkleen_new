29f2364833655f4bcebad8109f264c4487392bcd
Melvin-Emmanuel <emmanulmelv@gmail.com>
2023-11-17 08:05:58 +0100

    setup react flow

---
+++ unicorn/src/Routes/Allroutes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter} from "react-router-dom"
+import App from "../App"
+
+
+
+const Index = createBrowserRouter([
+    
+
+
+    {
+        path: "/",
+        index:true,
+        element: <App />,
+    
+    
+        
+    },
+  
+])
+
+export default Index
--- unicorn/src/main.tsx
+++ unicorn/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {RouterProvider} from "react-router-dom"
+import Index from './Routes/Allroutes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={Index}/>
   </React.StrictMode>,
 )
