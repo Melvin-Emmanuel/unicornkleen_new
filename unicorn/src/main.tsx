fce8d09f419ec999ceb6c715d12c782e43211cd8
Melvin-Emmanuel <emmanulmelv@gmail.com>
2023-12-13 02:04:43 +0100

    commited contact

--- unicorn/src/main.tsx
+++ unicorn/src/main.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import App from './App.tsx'
-import './index.css'
-import {RouterProvider} from "react-router-dom"
-import Index from './Routes/Allroutes.tsx'
-import {ToastContainer} from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-=======
->>>>>>> f314794e263fe1af8e2bc07c649f462b67f511a5
 
 import { RouterProvider } from 'react-router-dom'
 import Index from '../src/Routes/Allroutes'
@@ -20,12 +11,6 @@ import { Store } from './services/Stores'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <ToastContainer />
-   <RouterProvider router={Index}/>
-  </React.StrictMode>,
-)
-=======
     <Provider store={Store}>
   
       <RouterProvider router={Index} />
@@ -35,4 +20,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
 
                 
->>>>>>> f314794e263fe1af8e2bc07c649f462b67f511a5
